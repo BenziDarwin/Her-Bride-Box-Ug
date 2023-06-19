afc6a1f47d46f022f79a0dcd8fb119f5ac15d05e
NagabaBlessing <nagabablessing1@gmail.com>
2023-06-19 08:15:05 -0700

    commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -60,9 +60,9 @@ const pages = [
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
   borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  backgroundColor: green(theme.palette.common.white, 0.15),
   '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
+    backgroundColor: green(theme.palette.common.white, 0.25),
   },
   marginRight: theme.spacing(2),
   marginLeft: 0,
@@ -254,7 +254,7 @@ export default function NavBar() {
             component="div"
             sx={{ display: { xs: 'none', sm: 'block' } }}
           >
-            MUI
+            HerBrideBoxUg
           </Typography>
           <Search>
             <SearchIconWrapper>
