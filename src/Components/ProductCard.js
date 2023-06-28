0462d73e5771af56285b1a6f69c18037915b4d94
BenziDarwin <ssalibenjamin0402@gmail.com>
2023-06-28 10:55:20 +0300

    Fixed product image card

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -14,14 +14,16 @@ function ProductCard({src, itemName, price, itemId}) {
     format: '%v %s' // %s is the symbol and %v is the value
   });
   return (
-    <Link to={`/details/${itemId}`}>
+    
     <div class="max-w-sm xl:w-[500px] xs:w-[300px] m-12 text-left rounded overflow-hidden bg-[#E6E6E6] shadow-lg">
+      <Link to={`/details/${itemId}`}>
   <img class="w-full" src={`${src}`} alt="Item image"/>
   <div class="px-6 py-4">
     <p class="text-gray-700 text-base">
       {itemName}
     </p>
   </div>
+  </Link>
   <Grid container>
     <Grid item xs={7} md={6}>
     <span class="inline-block px-3 text-black py-1 text-lg font-semibold text-gray-700 mr-2 mb-2">{cprice}</span>
@@ -39,7 +41,7 @@ function ProductCard({src, itemName, price, itemId}) {
     null
     }
 </div>
-</Link>
+
   )
 }
 
