0218a45ba0011def71d920de93f73419520241e7
BenziDarwin <ssalibenjamin0402@gmail.com>
2023-06-29 22:30:45 +0300

    Fixed height

--- src/Components/OffersCard.js
+++ src/Components/OffersCard.js
@@ -5,7 +5,7 @@ function OffersCard({itemName, src, discount}) {
     <div class="max-w-sm bg-transparent m-12 xl:w-[500px]">
 <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">{itemName}</h5>
     <a href="#">
-        <img class="rounded-t-lg" src={src} alt="" />
+        <img class="rounded-t-lg h-[400px] w-auto" src={src} alt="" />
     </a>
     <div class="p-5">
         <a href="#">
